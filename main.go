b6666215874dfb84661352cba9f4843295e518b4
bloomingchad <antonloyed007@gmail.com>
2025-06-29 18:55:04 +0530

    use adrg/libvlc-go

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/bloomingchad/RadioHopper/internal/libmpv"
 	"github.com/rivo/tview"
+	"github.com/adrg/libvlc-go/v3"
 )
 
 func main() {
 	fmt.Println("RadioHopper is warming up...")
 
 	_ = tview.NewBox()
+    player, err := vlc.NewPlayer()
+    if err != nil {
+    	
+    }
+    defer player.Release()
+    
+    fmt.Println(player.IsPlaying())
 }
